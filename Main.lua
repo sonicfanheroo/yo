b3ec6f4f98aecd936795044bd23aa2acdfbc9693
sonicfanheroo <117123427+sonicfanheroo@users.noreply.github.com>
2022-10-31 22:27:10 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -18,7 +18,7 @@ video.Size = UDim2.new(1,0,1,0)
 local Name = "WHY_DID_AUSTIN_SEND_ME_THIS.mp4"
 
 if not isfile(Name) then
-    writefile(Name, game:HttpGet("https://github.com/sonicfanheroo/no-rick/blob/f34b5db883207e45c1104b75c6a75ac4e8385314/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20(Official%20Music%20Video).mp4"))
+    writefile(Name, game:HttpGet("https://github.com/sonicfanheroo/no-rick/blob/f34b5db883207e45c1104b75c6a75ac4e8385314/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20(Official%20Music%20Video).mp4?raw=true"))
 end
 
 video.Video = syn and getsynasset(Name) or getcustomasset(Name)
